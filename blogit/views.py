66ad36874654753beac03908c1937fa9c34119e0
AliceJoyce-aj <alicejoycechalakkal@gmail.com>
2024-12-27 23:52:47 +0530

    Add files via upload

--- blogit/views.py
+++ blogit/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect
+from . models import *
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def blogview(request):
+    return render(request,'blogview.html')
