3ec823c424693053fa535f0e985f1be14027a89e
AliceJoyce-aj <alicejoycechalakkal@gmail.com>
2025-01-02 20:30:27 +0530

    Add files via upload

--- blogit/models.py
+++ blogit/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class BlogDetails(models.Model):
+class BlogDetailss(models.Model):
+    uid=models.CharField(max_length=10)
     blog_title = models.CharField(max_length=100)
     blog_date = models.DateTimeField(auto_now_add=True)
     blog_author = models.CharField(max_length=100)
--- blogit/views.py
+++ blogit/views.py
@@ -2,60 +2,97 @@ from django.shortcuts import get_object_or_404, render,HttpResponse,redirect
 from . models import *
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import BlogDetails
+from .models import BlogDetailss
 from userapp . models import UserDetails
 from . models import *
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+
 # Create your views here.
 def index(request):
-    data = BlogDetails.objects.all()
-    return render(request,'index.html',{'result':data})
+    uid = request.session.get('uid') 
+    
+    if uid:
+        try:
+           
+            user = UserDetails.objects.get(id=uid)  
+            blog_author = user.username 
+            
+          
+            data = BlogDetailss.objects.filter(blog_author=blog_author)
+        except UserDetails.DoesNotExist:
+            data = BlogDetailss.objects.none()  
+    else:
+       
+        data = BlogDetailss.objects.all()
 
+    return render(request, 'index.html', {'result': data})
 
 def create_blog(request):
+    uid=request.session.get('uid')
     if request.method == 'POST':
+        
         blog_title = request.POST.get('blog_title')
         blog_content = request.POST.get('blog_content')
         blog_date = request.POST.get('blog_date')
-        data=BlogDetails.objects.create(blog_title=blog_title, blog_content=blog_content, blog_date=blog_date)
-        data.save()
+        blog_author = request.POST.get('blog_author')  
+
+
+       
+        BlogDetailss.objects.create(
+            uid=uid, 
+            blog_title=blog_title,
+            blog_content=blog_content,
+            blog_date=blog_date,
+            blog_author=blog_author  
+        )
     return render(request, 'createblog.html')
 
 
-def viewbloglist(request): 
-    data = BlogDetails.objects.filter(blog_author=UserDetails.username)
+def viewbloglist(request):
+    uid = request.session.get('uid')      
+    if uid:
+        try:
+            
+            user = UserDetails.objects.get(id=uid)  
+            blog_author = user.username  
+            
+          
+            data = BlogDetailss.objects.filter(blog_author=blog_author)
+        except UserDetails.DoesNotExist:
+            data = BlogDetailss.objects.none()  
+    else:
+        
+        data = BlogDetailss.objects.all()
+
     return render(request, 'bloglist.html', {'result': data})
 
-    
 def detailedview(request, id):
-    data = BlogDetails.objects.get(id=id)
+    data = get_object_or_404(BlogDetailss, id=id)  
     return render(request, 'detailed_view.html', {'result': data})
 
 
-def blogdelete(request,id):
-    data=BlogDetails.objects.get(pk=id)
+def blogdelete(request, id):
+    uid=request.session.get('uid')
+    data = get_object_or_404(BlogDetailss, id=id, uid=uid)
     data.delete()
-    return redirect('index')
+    return redirect('index')  
 
 
 def blogupdate(request, id):
-   
-    data = get_object_or_404(BlogDetails, id=id)
+    uid=request.session.get('uid')
+    data = get_object_or_404(BlogDetailss, id=id, uid=uid)
 
     if request.method == 'POST':
         blog_title = request.POST.get('blog_title')
         blog_date = request.POST.get('blog_date')
-        blog_author = request.POST.get('blog_author')
         blog_content = request.POST.get('blog_content')
 
-        
         data.blog_title = blog_title
         data.blog_date = blog_date
-        data.blog_author = blog_author
         data.blog_content = blog_content
         data.save()
 
-       
         return redirect('detailedview', id=data.id)
 
-    
     return render(request, 'update_blog.html', {'result': data})
